fc22707d5449f7460cc72cc0bfe33aa27c6d6350
AshishRawat08 <151005145+AshishRawat08@users.noreply.github.com>
2023-12-02 09:07:39 +0000

    UI

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ console.log("connected day 30");
 //     ]
 // }
 
-
+// backup storage 
 const state = {
   taskList: [],
 };
@@ -47,13 +47,14 @@ const taskModal = document.querySelector(".task_modal_body");
 // console.log(taskModal);
 
 // template for card on our screen
+ 
 // element identifier key is misssing on line 52 at the end -- key=${id}
-const htmlTaskContent = ({ id, title, type, description, url }) => `
+const htmlTaskContent = ({ id, title, description, type, url }) => `
     <div class ='col-md-6 col-lg-4 mt-3' id=${id}>
         <div class = 'card shadow-sm task_card'>
             <div class = 'card-header d-flex justify-content-end task_card_header'
                 <button type = 'button' class = 'btn btn-outline-light mr-1.5' name = '${id}'>
-                <i class="fas fa-pencil-alt name=${id}"></i>
+                <i class="fas fa-pencil-alt m-2 name=${id}"></i>
                 </button>
                 <button type = 'button' class = 'btn btn-outline-danger mr-1.5' name = '${id}'>
                 <i class="fas fa-trash-alt name=${id}"></i>
@@ -71,7 +72,7 @@ const htmlTaskContent = ({ id, title, type, description, url }) => `
                 </div>    
             </div>
             <div class ='card-footer'>
-                <button type = 'button'class = 'btn btn-outline-promary float-right' data-bs-toggle="modal" data-bs-target="#showTask">Open Task</button>
+                <button type = 'button'class = 'btn btn-outline-primary float-right' data-bs-toggle="modal" data-bs-target="#showTask">Open Task</button>
             </div>
         </div>
     </div>
@@ -93,12 +94,78 @@ const htmlModalContent = ({ id, title, description, url }) => {
     `;
 }
 
-
+// here we convert JSON to string
 const updatelocalstorage = () =>{
     localStorage.setItem(
-        "tasky",
+        "task",
         JSON.stringify({
-            tasks: state.taskList,  // calling state.taskList
+            task: state.taskList,  // calling state.taskList
         })
     );
 };
+
+
+// load  initial data 
+const LoadInitialData = () => {
+    const localStorageCopy = JSON.parse(localStorage.task)   // here we convert string to JSON for rendering card on screen 
+
+    if (localStorageCopy) state.taskList = localStorageCopy.task;
+
+    state.taskList.map((cardDate) =>{
+        taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(cardDate))
+    });
+};
+
+// spread operator 
+
+// const obj ={
+//     name : "ashish",
+//     age : 4
+// }
+// console.log(obj);
+//  {name: 'ashish', age: 4}
+// 
+// console.log({obj});
+// {obj: {…}}obj: {name: 'ashish', age: 4}age: 4name: "ashish"[[Prototype]]: Object
+// 
+// console.log({...obj});
+//  {name: 'ashish', age: 4}
+
+// adding a new key into object name designation
+// console.log({...obj, designation: "developer"});
+// {name: 'ashish', age: 4, designation: 'developer'}
+
+// updating key value using spread operator
+// console.log({...obj, age: 5});
+//  {name: 'khali', age: 5}
+
+// console.log({...obj, age: 6});
+//  {name: 'khali', age: 6}
+
+
+// encrypted date 
+// console.log(Date.now());
+// VM1190:1 1701500561611
+
+
+
+// when we upadte or when we edit ...we need to save 
+const handleSubmit = (event) =>{
+    // console.log("event triggered"); 
+    const id = `$(Date.now())`;
+    const input = {
+        url: document.getElementById("imageUrl").value,
+        title: document.getElementById("taskTitle").value,
+        tags: document.getElementById("tags").value,
+        taskDescription: document.getElementById("taskDescription").value,
+    };
+if (input.title ==="" || input.tags ==="" || input.taskDescription ==="") {
+       return alert("Please fill all the fields :-");
+    }
+
+taskContents.insertAdjacentHTML("beforeend",htmlTaskContent({...input, id}));  // using spread
+state.taskList.push({ ...input, id });
+updatelocalstorage();
+};
+
+
