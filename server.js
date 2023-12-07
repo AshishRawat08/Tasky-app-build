059068cf4c560deee2634e87e085b634dc8c2e81
AshishRawat08 <151005145+AshishRawat08@users.noreply.github.com>
2023-12-07 06:59:28 +0000

    todos

--- server.js
+++ server.js
@@ -12,31 +12,35 @@ const port = 8081; // local port numbner
 >> PUT: Full Update
 */
 
-const toDoList = ["learn", "apply things", "success"];
+const toDoList = ["learn", "apply things", "succeed"];
 
 http
   .createServer((req, res) => {
     // call back func
-    // const { method, url } = req;
+    const { method, url } = req;
 
-    // // console.log(method, url);
+    // console.log(method, url);
+    if (url === "/todos") {
+      if (method === "GET"){
+        res.writeHead(200);
+        res.write(toDoList.toString());
+      }
 
-    // if (url === "/todos") {
-    //   if (method === "GET") {
-    //     res.writeHead(200);
-    //     res.write(toDoList.toString());
-    //   }
-    // }
-    // res.end();
-    
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write("<h1>Hey my server started hello i am hereadhga</h1>");
+    }
     res.end();
 
-})
-.listen(port, () => {      // call back function 
+
+    // res.writeHead(200, {"Content-Type": "text/html"});
+    // res.write("<h1>Hey my server started hello i am hereadhga</h1>");
+    // res.end();
+
+  })
+  .listen(port, () => {      // call back function 
     console.log(`Node js server started running on port ${port}`);
-});
+  });
 
 // https://localhost:8081 
-// https://localhost:8081/
+// https://localhost:8081/#  called routs 
+// https://localhost:8081/sing-up
+// https://localhost:8081/about-us
+// https://localhost:8081/contact-us
